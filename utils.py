c773c3c47b46100c71127c10286e25003ec1d20c
ben_lab_server <ppingheng.chang@gmail.com>
2019-10-30 21:25:02 +0800

    label_ update

--- utils.py
+++ utils.py
@@ -81,7 +81,7 @@ def preprocess(path ,scale = 3):
     img = imread(path)
     label_ = img
     down_sampling_img = downSample(img, scale)
-    down_sampling_img = down_sampleing_img.astype(np.uint8)
+    down_sampling_img = down_sampling_img.astype(np.uint8)
 
     #bicbuic_img = cv2.resize(label_,None,fx = 1.0/scale ,fy = 1.0/scale, interpolation = cv2.INTER_CUBIC)# Resize by scaling factor
     input_ = cv2.resize(down_sampling_img,None,fx = scale ,fy=scale, interpolation = cv2.INTER_CUBIC)# Resize by scaling factor
